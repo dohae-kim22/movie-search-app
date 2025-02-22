d18c3f0eea1a87c0a03450edb32401ebb3ab6c55
DODO <dohae.kim22@gmail.com>
2025-02-22 23:44:12 +0100

    Add Headline

---
+++ src/components/Headline.js
@@ -0,0 +1,18 @@
+import { Component } from "../core/Component";
+
+export default class Headline extends Component {
+  render() {
+    this.el.classList.add("headline");
+    this.el.innerHTML = /* html */ `
+    <h1>
+        <span>OMDb API:</span><br/>
+        SEARCH AND DISCOVER<br/>
+        MOVIES IN ONE PLACE
+    </h1>
+    <p>
+        <span>Access Movie Information Instantly, Powered by OMDb API</span><br/>
+        Easily search for movies, view ratings, posters, and more, all in one seamless experience. All content is contributed and maintained by movie enthusiasts.
+    </p>
+    `;
+  }
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,11 @@
 import { Component } from "../core/Component";
+import Headline from "../components/Headline";
 
 export default class Home extends Component {
   render() {
-    this.el.innerHTML = "<h1>HELLO</h1>";
+    const headline = new Headline().el;
+
+    this.el.classList.add("container");
+    this.el.appendChild(headline);
   }
 }
