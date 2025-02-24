ea2d01a4714af206988b862954789154b6f1828d
DODO <dohae.kim22@gmail.com>
2025-02-24 22:37:10 +0100

    Add About & Not Found Page

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+import Store from "../core/store";
+
+const store = new Store({
+  image:
+    "https://avatars.githubusercontent.com/u/149057689?s=400&u=1dc5975e259e94249cfe8c1adc47a0c66a4258d9&v=4",
+  name: "KIMII / DoHae KIM",
+  email: "dohae.kim22@gmail.com",
+  github: "https://github.com/dohae-kim22/movie-search-app",
+});
+
+export default store;
